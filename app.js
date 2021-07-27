009b4ebf750cfdf3b5324189ca9f5a854d4d8d61
EvgenStr <juve2910@gmail.com>
2021-07-27 22:44:44 +0300

    fix import problems in app and index

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = require('./routes');
 const { STATIC_PATH } = require('./config/config');
-const errorHandler = require('./middlewares/error.handlers.mw');
+// const errorHandler = require('./middlewares/error.handlers.mw');
 
 const app = express();
 app.use(express.static(STATIC_PATH));
 app.use(express.json()); 
 app.use('/api', router);
-app.use(errorHandler);
+// app.use(errorHandler);
 
 module.exports = app;
