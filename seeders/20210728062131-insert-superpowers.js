e5cf59d0d411dc2015fe826883716a4aed15ee83
EvgenStr <juve2910@gmail.com>
2021-07-28 10:20:42 +0300

    add superpower seed

---
+++ seeders/20210728062131-insert-superpowers.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'superpowers',
+      [
+        { superpower: 'sleep-test', created_at: new Date(), updated_at: new Date() },
+        { superpower: 'eat', created_at: new Date(), updated_at: new Date() },
+        { superpower: 'fly', created_at: new Date(), updated_at: new Date() },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
