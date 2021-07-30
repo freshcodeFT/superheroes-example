8358c99fe59269aaee2355df98d1aa4345741d94
Nikolay <freshitftm@gmail.com>
2021-07-30 12:30:23 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 const { STATIC_PATH } = require('./config/config');
 const errorHandler = require('./middlewares/error.handlers.mw');
 
 const app = express();
+app.use(cors());
 app.use(express.static(STATIC_PATH));
-app.use(express.json()); 
+app.use(express.json());
 app.use('/api', router);
 app.use(errorHandler);
 
